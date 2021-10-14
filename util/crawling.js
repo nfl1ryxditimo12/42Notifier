95071d48eebb447fb16f014807800db6532071a3
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-10-15 00:01:48 +0900

    헤로쿠 puppeteer 오류 수정

--- util/crawling.js
+++ util/crawling.js
@@ -2,7 +2,9 @@ const puppeteer = require("puppeteer");
 
 module.exports = async (eventId) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
         const page = await browser.newPage();
         const login = process.env.FT_LOGIN;
         const password = process.env.FT_PASSWORD;
