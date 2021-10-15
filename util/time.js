7f236dcf938a157a01aef6b64dd551ff59e9680f
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-10-15 22:12:21 +0900

    시간 포맷 변경

--- util/time.js
+++ util/time.js
@@ -1,12 +1,15 @@
 module.exports = (time) => {
+    const week = ["월", "화", "수", "목", "금", "토", "일"];
     const utc = new Date(time);
     const year = utc.getFullYear();
     const month = utc.getMonth();
     const day = utc.getDate();
+    const today = new Date(`${year}-${month + 1}-${day}`).getDay();
+    const weekLabel = week[today];
     const minute = utc.getMinutes();
     const utcHour = utc.getHours() + 9;
     const hour = utcHour >= 13 ? utcHour - 12 : utcHour;
     const division = utcHour >= 12 ? "오후" : "오전";
 
-    return `${year}년 ${month + 1}월 ${day}일 - ${division} ${hour}시 ${minute}분`;
+    return `${year}년 ${month + 1}월 ${day}일(${weekLabel})  ${division} ${hour}시 ${minute}분`;
 };
