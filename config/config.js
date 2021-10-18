3036e42c9f9ea24d6f2afc43f0cecbe7eecbacb0
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-10-18 18:05:22 +0900

    개발 / 서버 환경 분리

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
+if (process.env.SERVER_ENV === "development") require("dotenv").config();
+
 module.exports = {
     development: {
         username: "root",
         password: null,
-        database: "database_development",
+        database: "42Alert",
         host: "127.0.0.1",
         dialect: "mysql",
+        logging: false,
     },
     test: {
         username: "root",
