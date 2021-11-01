f7bb30530ab6d55cb52c2ecab334bfaea96fe59d
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-11-01 21:29:14 +0900

    비슷한 로직 통일

--- modules/getNewData.js
+++ modules/getNewData.js
@@ -1,29 +1,22 @@
 const Event = require("../models/event");
 const Exam = require("../models/exam");
 
-const newEvent = async (data) => {
+module.exports = async (data, flag) => {
     const eventApi = data;
     eventApi.sort((a, b) => b.id - a.id);
-    const nowEvent = await Event.findAll({});
+    const nowEvent = flag === "event" ? await Event.findAll({}) : await Exam.findAll({});
     nowEvent.sort((a, b) => b.dataValues.id - a.dataValues.id);
 
-    return eventApi.filter((event) => event.id > nowEvent[0].dataValues.id);
+    if (flag === "event") {
+        return eventApi.filter((event) => event.id > nowEvent[0].dataValues.id);
+    } else {
+        return eventApi.filter((event) => {
+            if (
+                event.cursus[0].slug === "42cursus" &&
+                event.name.indexOf("test") === -1 &&
+                event.location.indexOf("test") === -1
+            )
+                return event.id > nowEvent[0].dataValues.id;
+        });
+    }
 };
-
-const newExam = async (data) => {
-    const eventApi = data;
-    eventApi.sort((a, b) => b.id - a.id);
-    const nowEvent = await Exam.findAll({});
-    nowEvent.sort((a, b) => b.dataValues.id - a.dataValues.id);
-
-    return eventApi.filter((event) => {
-        if (
-            event.cursus[0].slug === "42cursus" &&
-            event.name.indexOf("test") === -1 &&
-            event.location.indexOf("test") === -1
-        )
-            return event.id > nowEvent[0].dataValues.id;
-    });
-};
-
-module.exports = { newEvent, newExam };
